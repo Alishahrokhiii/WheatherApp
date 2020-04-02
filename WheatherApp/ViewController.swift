f4653edc0f70707ac8794dd647864790a624d3e0
Seyed Ali Shahrokhi <Alishahrokhi@icloud.com>
2020-04-02 15:49:47 +0430

    Version 1.0.0

--- WheatherApp/ViewController.swift
+++ WheatherApp/ViewController.swift
@@ -10,9 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LocationLable: UILabel!
+    @IBOutlet weak var DayLable: UILabel!
+    @IBOutlet weak var ConditionImageView: UIImageView!
+    @IBOutlet weak var ConditionLable: UILabel!
+    @IBOutlet weak var ImageLable: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+        
+      let apiKey = "e6057df6d4ee3afb8ec812e780d488b4"
+    var lat = 33.487782
+    var lon = 48.355831
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Khorramabad,state,IR&units=metric&appid=\(apiKey)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, erorr) in
+            if let data = data, erorr == nil {
+            
+            do {
+                
+            
+               guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)as? [String : Any] else {return}
+                
+               guard let weatherDetails = dictionary["weather"]as? [[String : Any]],
+                     let weatherMain = dictionary["main"] as? [String : Any] else {return}
+                
+                
+                let CityName = dictionary["name"]
+                let discription = (weatherDetails.first?["description"] as? String)
+                let Temp = weatherMain["temp"] as? Double
+                let temp = Int(Temp!)
+                
+ 
+                DispatchQueue.main.async {
+                    self.LocationLable.text = (CityName as! String)
+                    self.ConditionLable.text = (discription)
+                    self.ImageLable.text = (String(temp))
+                }
+             
+                    
+              
+            } catch {
+                print("we had an erorr retriving data")
+                
+                
+            }
+        }
+        }
+        task.resume()
+      
+        
+        
     }
 
 
